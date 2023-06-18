edf41279f1e44ab2f230c2089560e53c493752eb
Damith Malalanayake <damith@sysensor.org>
2023-06-18 23:13:27 +0530

    add todos, and write readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # rss
 Spring boot rss feed demo project
+
+###Design
+JDK - Java 17  
+DB - H2 in-memory database
+
+###Build
+-mvn clean install -Dspring.profiles.active=test
+
+###Run
+-mvn spring-boot:run  
---
+++ src/main/java/com/getgifted/rss/api/RssFeedAPI.java
@@ -0,0 +1,11 @@
+package com.getgifted.rss.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/rss")
+public class RssFeedAPI {
+
+// TODO: write apis to expose rss items
+}
