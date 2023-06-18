990989ca56e77d3d9d7f67e4df6fa77866a9b178
Damith Malalanayake <damith@sysensor.org>
2023-06-18 23:24:04 +0530

    format code

--- src/main/java/com/getgifted/rss/RssApplication.java
+++ src/main/java/com/getgifted/rss/RssApplication.java
@@ -8,8 +8,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class RssApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RssApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RssApplication.class, args);
+    }
 
 }
